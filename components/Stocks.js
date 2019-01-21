2023b68b16fd9037f1c1d9a78502bdd23c08f4ee
Daniel Park <dpeace926@gmail.com>
2019-01-21 16:01:56 -0500

    add loader

--- components/Stocks.js
+++ components/Stocks.js
@@ -2,6 +2,7 @@ import React, { Component, Fragment } from "react";
 import Link from "next/link";
 import { keys } from "../config/keys";
 import "../styles/stocks.scss";
+import Loader from "react-loader";
 
 class Stocks extends Component {
   constructor() {
@@ -28,18 +29,42 @@ class Stocks extends Component {
   render() {
     const { cryptoNewsData } = this.state;
 
+    console.log(cryptoNewsData);
+
     return (
       <div className="col-6">
+        {!cryptoNewsData && (
+          <div className="relative p4">
+            <Loader color="#fff" />
+          </div>
+        )}
         {cryptoNewsData &&
           cryptoNewsData.results.map((news, idx) => {
             return (
               <div key={idx} className="stocks-item p1">
                 <Link href={news.url}>
-                  <a>
+                  <a target="_blank">
                     {news.title}
                     <span className="nowrap">{` - ${news.source.domain}`}</span>
                   </a>
                 </Link>
+                <div>
+                  {news.votes.liked > 0 && (
+                    <span className="pr2 green">{news.votes.liked}</span>
+                  )}
+                  {news.votes.important > 0 && (
+                    <span className="pr2">{news.votes.important}</span>
+                  )}
+                  {news.votes.disliked > 0 && (
+                    <span className="pr2 red">{news.votes.disliked}</span>
+                  )}
+                  {news.votes.positive > 0 && (
+                    <span className="pr2 blue">{news.votes.positive}</span>
+                  )}
+                  {news.votes.negative > 0 && (
+                    <span className="pr2 red">{news.votes.negative}</span>
+                  )}
+                </div>
               </div>
             );
           })}
--- components/TopNews.js
+++ components/TopNews.js
@@ -2,6 +2,7 @@ import React, { Component, Fragment } from "react";
 import Link from "next/link";
 import { keys } from "../config/keys";
 import "../styles/topnews.scss";
+import Loader from "react-loader";
 
 class TopNews extends Component {
   constructor() {
@@ -29,12 +30,17 @@ class TopNews extends Component {
 
     return (
       <div className="news-section col-6">
+        {!newsData && (
+          <div className="relative p4">
+            <Loader color="#fff" />
+          </div>
+        )}
         {newsData &&
           newsData.articles.map((news, idx) => {
             return (
               <div key={idx} className="news-item p1">
                 <Link href={news.url}>
-                  <a>{news.title}</a>
+                  <a target="_blank">{news.title}</a>
                 </Link>
               </div>
             );
