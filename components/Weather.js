a60f583c097e30035ba9a1067335afe1e7bfa064
Daniel Park <dpeace926@gmail.com>
2019-01-23 17:13:23 -0500

    import weather icons

--- components/Weather.js
+++ components/Weather.js
@@ -1,6 +1,22 @@
 import React, { Component, Fragment } from "react";
 import Link from "next/link";
 import WbCloudy from "rmdi/lib/WbCloudy";
+import Sunny from "../static/iconfinder_sunny_2671740.svg";
+import Night from "../static/iconfinder_night_2671741.svg";
+import Windy from "../static/iconfinder_windy_2671739.svg";
+import Cloudy from "../static/iconfinder_cloudy_2671738.svg";
+import CloudyRainy from "../static/iconfinder_cloudy_rainy_2671734.svg";
+import CloudyStorm from "../static/iconfinder_cloudy_storm_2671744.svg";
+import CloudyNightSnowyRainy from "../static/iconfinder_cloudy_night_snowy_rainy_2671735.svg";
+import CloudyDay from "../static/iconfinder_cloudy_day_2671737.svg";
+import CloudyRainyDay from "../static/iconfinder_cloudy_rainy_day_2671749.svg";
+import CloudyStormyDay from "../static/iconfinder_cloudy_stormy_day_2671743.svg";
+import CloudySnowyDay from "../static/iconfinder_cloudy_snowy_day_2671746.svg";
+import CloudyNight from "../static/iconfinder_cloudy_night_2671736.svg";
+import CloudyRainyNight from "../static/iconfinder_cloudy_rainy_night_2671748.svg";
+import CloudyStormyNight from "../static/iconfinder_cloudy_stormy_night_2671742.svg";
+import CloudySnowyNight from "../static/iconfinder_cloudy_snowy_day_2671746.svg";
+import CloudyRainySnowyNight from "../static/iconfinder_cloudy_rainy_snowy_night_2671747.svg";
 import { Days } from "../src/date";
 import { keys } from "../config/keys";
 import "../styles/styles.scss";
@@ -49,6 +65,26 @@ class Weather extends Component {
     const fiveDayForecast = {};
     let newArr = [];
 
+    const weatherIcons = {
+      "01d": Sunny,
+      "02d": CloudyDay,
+      "03d": Cloudy,
+      "04d": Cloudy,
+      "09d": CloudyRainyDay,
+      "10d": CloudyRainyDay,
+      "11d": CloudyStormyDay,
+      "13d": CloudySnowyDay,
+      "01n": Night,
+      "02n": CloudyNight,
+      "03n": CloudyNight,
+      "04n": CloudyNight,
+      "09n": CloudyRainyNight,
+      "10n": CloudyRainyNight,
+      "11n": CloudyStormyNight,
+      "13n": CloudySnowyNight,
+      "50n": Windy
+    };
+
     if (forecastData) {
       for (let i = 0; i < forecastData.list.length; i++) {
         let forecast = forecastData.list[i];
@@ -65,7 +101,8 @@ class Weather extends Component {
             ["dt"]: forecast.dt_txt,
             ["high"]: forecast.main.temp,
             ["low"]: 0,
-            ["description"]: forecast.weather[0].description
+            ["description"]: forecast.weather[0].description,
+            ["icon"]: forecast.weather[0].icon
           };
         }
       }
@@ -84,7 +121,11 @@ class Weather extends Component {
               {weatherData.weather[0].description}
             </div>
             <div>
-              <WbCloudy size={52} color="#9d9d9f" />
+              <img
+                src={weatherIcons[weatherData.weather[0].icon]}
+                width={52}
+                height={52}
+              />
             </div>
             <div className="h2">{Math.round(weatherData.main.temp)}°</div>
             <p className="flex justify-center m0 light-gray">
@@ -113,7 +154,11 @@ class Weather extends Component {
                     {forecast.description}
                   </p>
                   <div className="py1">
-                    <WbCloudy size={52} color="#9d9d9f" />
+                    <img
+                      src={weatherIcons[forecast.icon]}
+                      width={52}
+                      height={52}
+                    />
                   </div>
                   <p className="m0">
                     <span className="p1">{Math.round(forecast.high)}°</span>
