5c3d5924316d17520dd00c0f9d5625c1f0b188a0
Daniel Park <dpeace926@gmail.com>
2019-05-02 23:07:08 -0400

    tweak

---
+++ config/keys.js
@@ -0,0 +1,12 @@
+export const keys = {
+  proxyURL: process.env.proxyURL,
+  openWeatherMapAPI: process.env.openWeatherMapAPI,
+  openWeatherMapAPIKey: process.env.openWeatherMapAPIKey,
+  cryptoCompareAPI: process.env.cryptoCompareAPI,
+  cryptoCompareAPIKey: process.env.cryptoCompareAPIKey,
+  newsAPI: process.env.newsAPI,
+  newsAPIKey: process.env.newsAPIKey,
+  cryptoPanicAPI: process.env.cryptoPanicAPI,
+  cryptoPanicAPIKey: process.env.cryptoPanicAPIKey,
+  db: process.env.db
+};
