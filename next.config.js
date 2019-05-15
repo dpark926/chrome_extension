0e4b615f4430ff1c115c8f95e76e688398b672d8
Daniel Park <dpeace926@gmail.com>
2019-05-15 19:44:02 -0400

    yo

--- next.config.js
+++ next.config.js
@@ -1,44 +1,41 @@
-const withSass = require("@zeit/next-sass");
-const withImages = require("next-images");
-
-const { parsed: localEnv } = require("dotenv").config();
-const webpack = require("webpack");
-
-module.exports = withSass(
-  withImages({
-    // webpack(config) {
-    //   config.plugins.push(new webpack.EnvironmentPlugin(localEnv));
-    //
-    //   return config;
-    // }
-    env: {
-      PROXY_URL: process.env.PROXY_URL
-    }
-  })
-);
-
-// require("dotenv").config();
 // const withSass = require("@zeit/next-sass");
 // const withImages = require("next-images");
-// const path = require("path");
-// const Dotenv = require("dotenv-webpack");
+//
+// const { parsed: localEnv } = require("dotenv").config();
+// const webpack = require("webpack");
 //
 // module.exports = withSass(
 //   withImages({
-//     webpack: config => {
-//       config.plugins = config.plugins || [];
-//
-//       config.plugins = [
-//         ...config.plugins,
-//
-//         // Read the .env file
-//         new Dotenv({
-//           path: path.join(__dirname, ".env"),
-//           systemvars: true
-//         })
-//       ];
+//     webpack(config) {
+//       config.plugins.push(new webpack.EnvironmentPlugin(localEnv));
 //
 //       return config;
 //     }
 //   })
 // );
+
+require("dotenv").config();
+const withSass = require("@zeit/next-sass");
+const withImages = require("next-images");
+const path = require("path");
+const Dotenv = require("dotenv-webpack");
+
+module.exports = withSass(
+  withImages({
+    webpack: config => {
+      config.plugins = config.plugins || [];
+
+      config.plugins = [
+        ...config.plugins,
+
+        // Read the .env file
+        new Dotenv({
+          path: path.join(__dirname, ".env"),
+          systemvars: true
+        })
+      ];
+
+      return config;
+    }
+  })
+);
