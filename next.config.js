98898eefaa65ef7fcbdeeca8d293f3d01bf4b953
Daniel Park <dpeace926@gmail.com>
2019-05-15 13:14:23 -0400

    yo

--- next.config.js
+++ next.config.js
@@ -16,23 +16,26 @@
 
 require("dotenv").config();
 const withSass = require("@zeit/next-sass");
+const withImages = require("next-images");
 const path = require("path");
 const Dotenv = require("dotenv-webpack");
 
-module.exports = withSass({
-  webpack: config => {
-    config.plugins = config.plugins || [];
+module.exports = withSass(
+  withImages({
+    webpack: config => {
+      config.plugins = config.plugins || [];
 
-    config.plugins = [
-      ...config.plugins,
+      config.plugins = [
+        ...config.plugins,
 
-      // Read the .env file
-      new Dotenv({
-        path: path.join(__dirname, ".env"),
-        systemvars: true
-      })
-    ];
+        // Read the .env file
+        new Dotenv({
+          path: path.join(__dirname, ".env"),
+          systemvars: true
+        })
+      ];
 
-    return config;
-  }
-});
+      return config;
+    }
+  })
+);
