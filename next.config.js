b3ba9c5fb6e98d8c9e7d47bb8461e8b49ecb9d41
Daniel Park <dpeace926@gmail.com>
2019-05-15 20:34:22 -0400

    heroku config vars

--- next.config.js
+++ next.config.js
@@ -20,22 +20,4 @@ const withImages = require("next-images");
 const path = require("path");
 const Dotenv = require("dotenv-webpack");
 
-module.exports = withSass(
-  withImages({
-    webpack: config => {
-      config.plugins = config.plugins || [];
-
-      config.plugins = [
-        ...config.plugins,
-
-        // Read the .env file
-        new Dotenv({
-          path: path.join(__dirname, ".env"),
-          systemvars: true
-        })
-      ];
-
-      return config;
-    }
-  })
-);
+module.exports = withSass(withImages());
